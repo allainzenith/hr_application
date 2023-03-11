5cfd73c5549093436272616945f8c0a2dc615a10
unknown <NN112@ccs.local>
2023-03-11 00:09:21 -0800

    commit

--- src/main/java/ph/com/alliance/jpa/functions/employee/service/EmployeeService.java
+++ src/main/java/ph/com/alliance/jpa/functions/employee/service/EmployeeService.java
@@ -1,5 +1,12 @@
 package ph.com.alliance.jpa.functions.employee.service;
 
+import ph.com.alliance.jpa.entity.Employee;
+
 public interface EmployeeService {
 	Object getAllEmployees();
+	
+	void createEmployee(Employee employeemodel);
+	void updateEmployee(Integer empID, Employee employeemodel);
+	void deleteEmployee(Integer empID);
+	Object findByDepartment(String department);
 }
--- src/main/java/ph/com/alliance/jpa/functions/ticket/service/TicketService.java
+++ src/main/java/ph/com/alliance/jpa/functions/ticket/service/TicketService.java
@@ -6,7 +6,12 @@ public interface TicketService {
 	Object getAllTickets();
 	
 	void createTicket(Ticket ticketmodel);
-	
+	/**
+	 * This is a very awesome documentation
+	 * @param TicketModel - model
+	 * **/
 	void updateTicket(Integer ticketID, Ticket ticket);
+	void deleteTicket(Integer ticketID);
+	Object findByStatus(String status);
 	
 }
